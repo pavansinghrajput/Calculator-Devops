64679802f45d47ac9f7ee87785dcd25c5cf87013
Pavan Kumar Singh <pavansingh9900@gmail.com>
2021-03-13 20:25:11 +0530

    dockerfile updated

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,25 +4,73 @@ import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 //line added
-public class App
-{
-//line added
-private static final Logger logger = LogManager.getLogger(App.class);
-//line added
+public class App {
+    //line added
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    //line added
     //
-    public static void main(String args[])
-    {
-        int choice, flag=0;
-        double num1,num2;
+    public static void main(String args[]) {
+        int n;
+        //double num1,num2;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Calculator");
-        do {
-            System.out.println("1 for Addition\n2 for Subtraction\n3 for Multiplication\n4 for Division\n");
+        System.out.println("=========================================================================================");
+        System.out.println("                                 Scientific Calculator                                   ");
+        System.out.println("=========================================================================================");
+        while (true) {
+            // System.out.println("Select anyone among 5 options");
+            System.out.println("1) for Square Root,Enter 1\n2 for Factorial,Enter 2\n3 for Log base e,Enter 3\n4 for Power Function,Enter 4\n5) for EXit,Enter 5");
             System.out.print("Enter your choice: ");
-            choice = scan.nextInt();
+            n = scan.nextInt();
+
+
+            if (n == 1) {
+                System.out.println("Enter number for Square Root:");
+
+                double a = scan.nextDouble();
+                double res = Sqeroot(a);
+                System.out.println("The Square root of " + a + "is" + res);
+
+            } else if (n == 2) {
+                System.out.println("Enter number for Factorial:");
+
+                Long b = scan.nextLong();
+                double res = Fact(b);
+                if(res>0)
+                {
+                    System.out.println("The Factorial of " + b + "is" + res);
+                }
+
+            } else if (n == 3) {
+                System.out.println("Enter number for Log:");
+
+                double a = scan.nextDouble();
+                double res = Loge(a);
+                System.out.println("The Log of " + a + "is" + res);
+
+
+            } else if (n == 4) {
+                System.out.println("Enter first number :");
+
+                double a = scan.nextDouble();
+                System.out.println("Enter second number :");
+
+                double b = scan.nextDouble();
+
+                double res = Powe(a,b);
+                System.out.println("Power of " + a + "and "+b+"is" + res);
+            } else if (n == 5) {
+                System.out.println("Ending the Program");
+                logger.info("Entered 5 to exit.");
+                return ;
+
+            } else {
+                System.out.println("PLease Enter Valid Option");
+            }
+            /*
             if (choice > 4 || choice <1) {
                 flag = 1;
-                System.out.println("Invalid choice, exiting\n");
+                System.out.println("Incorrect Option Choosed, Ending the Program\n");
             }
             else if (flag != 1){
             	 System.out.println("Enter two numbers");
@@ -45,13 +93,17 @@ private static final Logger logger = LogManager.getLogger(App.class);
                 }
             }
             System.out.println("\n");
-            }while(flag==0);
+            }while(flag==0);*/
+        }
+
     }
+
     //logger.error("print an error");
     //logger.info("print some info");
-    static double Add(double a, double b) {
-    logger.info("Adding two numbers"+a +"and"+b);
-    	return a + b;
+    /*static double (double a) {
+    logger.info("Adding two numbers"+a +"and");
+
+    	return a ;
     }
     static double Subtract(double a, double b) {
     	return a - b;
@@ -66,3 +118,56 @@ private static final Logger logger = LogManager.getLogger(App.class);
     	else return a / b;
     }
 }
+*/
+    static double Sqeroot(double a) {
+
+        double res = Math.sqrt(a);
+        logger.info("Square root of " + a + "is" + res);
+
+        return res;
+    }
+
+    static long Fact(long b) {
+
+        //double res = Math.sqrt(a);
+        long res=1;
+        if(b<0) {
+                System.out.println("Enter number >= 0");
+            logger.info("Number entered for Square root is negative.");
+            return -1;
+                }
+        else {
+            for (long i = b; i > 0; i--) {
+                res = res * i;
+            }
+            logger.info("Factorial of " + b + "is" + res);
+            return res;
+        }
+
+
+
+        }
+
+
+
+    static double Loge(double a) {
+
+        double res = Math.log(a);
+        logger.info("Log of " + a + "is" + res);
+
+        return res;
+    }
+    static double Powe(double a,double b) {
+
+        double res = Math.pow(a,b);
+        logger.info("Power of " + a + "and "+b+"is" + res);
+
+        return res;
+    }
+
+
+
+
+    }
+
+
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -14,45 +14,47 @@ public class CalculatorTest {
 	    }
 
 		@Test
-	    public void testAdd() {
-	        double a = 15.0;
-	        double b = 20.0;
-	        double expectedResult = 35.0;
-	        double result = calculator.Add(a, b);
+	    public void testSqeroot() {
+	        double a = 4.0;
+
+	        double expectedResult = 2.0;
+	        double result = calculator.Sqeroot(a);
 	        Assert.assertEquals(expectedResult, result, 0);
 	    }
 		
 		@Test
-		public void testSubtract() {
-			double a = 34;
-	        double b = 12;
-	        double expectedResult = 22;
-	        double result = calculator.Subtract(a, b);
+		public void testFact() {
+			long a = 10;
+
+	        long expectedResult = 3628800;
+	        double result = calculator.Fact(a);
+	        Assert.assertEquals(expectedResult, result, 0);
+		}
+
+	@Test
+	public void testLoge() {
+		double a = 1;
+
+		double expectedResult = 0.0;
+		double result = calculator.Loge(a);
+		Assert.assertEquals(expectedResult, result, 0.0001);
+	}
+		
+		@Test
+		public void testPowe() {
+			double a = 7;
+	        double b = 3;
+	        double expectedResult = 343;
+	        double result = calculator.Powe(a, b);
 	        Assert.assertEquals(expectedResult, result, 0);
 		}
 		
-		@Test
-		public void testMultiply() {
-			double a = 18.5;
-	        double b = 12.6;
-	        double expectedResult = 233.1;
-	        double result = calculator.Multiply(a, b);
-	        Assert.assertEquals(expectedResult, result, 0);
-		}
+
 		
-		@Test
-		public void testDivide() {
-			double a = 60.0;
-	        double b = 12.0;
-	        double expectedResult = 5.0;
-	        double result = calculator.Divide(a, b);
-	        Assert.assertEquals(expectedResult, result, 0.0001);
-		}
-		
-		@Test(expected = IllegalArgumentException.class)
+		/*@Test(expected = IllegalArgumentException.class)
 	    public void testDivideByZero() {
 	        int a = 25;
 	        int b = 0;
 	        calculator.Divide(a, b);
-	    }
+	    }*/
 }
